78b5c00fca254bb7f6ad9712225db8cf841bb191
bogdzn <wontsleep@protonmail.com>
2021-11-08 13:10:10 +0100

    (mod/ast)support for functions and primitives

--- src/Tokens.hs
+++ src/Tokens.hs
@@ -1,10 +1,17 @@
 module Tokens where
 
+import ParserLib
+import Data.IORef
+
+type Env = IORef [(String, IORef Token)]
 data Token = Number Integer |
     Bool Bool |
     String String |
     Atom String |
-    List [Token]
+    List [Token] |
+    Primitive ([Token] -> Either ParserError Token) |
+    Lambda { _params :: [Token], _args :: Maybe Token,
+        _body :: [Token], _env :: Env }
 
 instance Show Token where
     show (Bool True)            = "#t"
@@ -13,6 +20,8 @@ instance Show Token where
     show (Atom s)               = ":" ++ s                  -- Like in Elixir
     show (String s)             = s
     show (List x)               = "(" ++ unlist x ++ ")"
+    show (Primitive _)          = "<primitive>"
+    show Lambda {_params = p}   = "<lambda (" ++ unlist p ++ ")."
 
 unlist :: [Token] -> String
 unlist = unwords . map show
